7f894263d6216ba94b449dc8bdf42e672945cb6b
mmarfat <markec_marf@hotmail.com>
2025-04-16 21:18:45 +0200

    chore(localization): remove Suspense as react-i18next handles it by default

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 // React
-import { StrictMode, Suspense } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 // Shadcn
@@ -17,10 +17,8 @@ import '@fontsource-variable/inter';
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Suspense fallback={<div>Loading...</div>}>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </Suspense>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
