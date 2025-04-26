8bb6b6eaac207a81693c7c097b6c44b8378234e8
mmarfat <markec_marf@hotmail.com>
2025-04-26 16:48:54 +0200

    fix: adjust size of center dots preview to more closely resemble the corner dots preview

--- src/components/stylingPreviews/centers/CenterDots/index.jsx
+++ src/components/stylingPreviews/centers/CenterDots/index.jsx
@@ -1,18 +1,18 @@
 const CenterDots = ({ color }) => (
   <svg width="42" height="42" viewBox="0 0 42 42" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="6" cy="6" r="6" fill={color || "#000000"} />
-    <circle cx="18" cy="6" r="6" fill={color || "#000000"} />
-    <circle cx="30" cy="6" r="6" fill={color || "#000000"} />
+    <circle cx="7" cy="7" r="5" fill={color || "#000000"} />
+    <circle cx="21" cy="7" r="5" fill={color || "#000000"} />
+    <circle cx="35" cy="7" r="5" fill={color || "#000000"} />
 
-    <circle cx="30" cy="18" r="6" fill={color || "#000000"} />
-    <circle cx="30" cy="30" r="6" fill={color || "#000000"} />
+    <circle cx="35" cy="21" r="5" fill={color || "#000000"} />
+    <circle cx="35" cy="35" r="5" fill={color || "#000000"} />
     
-    <circle cx="18" cy="30" r="6" fill={color || "#000000"} />
-    <circle cx="6" cy="30" r="6" fill={color || "#000000"} />
+    <circle cx="21" cy="35" r="5" fill={color || "#000000"} />
+    <circle cx="7" cy="35" r="5" fill={color || "#000000"} />
     
-    <circle cx="6" cy="18" r="6" fill={color || "#000000"} />
+    <circle cx="7" cy="21" r="5" fill={color || "#000000"} />
 
-    <circle cx="18" cy="18" r="6" fill={color || "#000000"} />
+    <circle cx="21" cy="21" r="5" fill={color || "#000000"} />
     
   </svg>
 );
