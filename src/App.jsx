057e3bc53a06cc44b3bdbfdcff429f66319068b6
mmarfat <markec_marf@hotmail.com>
2025-04-28 20:46:10 +0200

    chore: added lazy loading for smaller chunk sizes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import Main from "@/components/layout/Main";
-import Header from "@/components/layout/Header";
+// Lazy load to reduce chunk size
+import { lazy, Suspense } from 'react';
+
+const Header = lazy(() => import("@/components/layout/Header"))
+const Main = lazy(() => import("@/components/layout/Main"))
 
 const App = () => {
   return (
-    <div className="w-full mx-auto px-4 py-4 flex flex-col gap-4 lg:max-w-7xl xl:px-0 md:gap-10">
-      <Header/>
-      <Main/>
-    </div>
+    <Suspense>
+      <div className="w-full mx-auto px-4 py-4 flex flex-col gap-4 lg:max-w-7xl xl:px-0 md:gap-10">
+        <Header/>
+        <Main/>
+      </div>
+    </Suspense>
   );
 };
 
