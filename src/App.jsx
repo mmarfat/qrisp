0964b8728ce68e079d6c4e2bdfd3586cd9ebc4fd
mmarfat <markec_marf@hotmail.com>
2025-04-28 21:57:32 +0200

    perf: removed lazy loading as it doesn't provide much in our case and causes visual flicker on initial load

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,14 @@
+import Main from '@/components/layout/Main';
 import Header from '@/components/layout/Header';
-
-// i18n
-import { useTranslation } from 'react-i18next';
-
-// Lazy load to reduce chunk size
-import { lazy, Suspense } from 'react';
-
-const Main = lazy(() => import("@/components/layout/Main"))
-const Footer = lazy(() => import("@/components/layout/Footer"))
+import Footer from '@/components/layout/Footer';
 
 const App = () => {
-
-  const { t } = useTranslation();
-
   return (
     <div className="w-full mx-auto px-4 pt-4 flex flex-col gap-4 lg:max-w-7xl xl:px-0 md:gap-10">
       <Header />
       <div>
-        <main className="flex flex-col items-center justify-center gap-4 md:gap-10">
-          <div className="flex flex-col items-center justify-center text-center gap-4 hidden md:flex">
-            <h1 className="text-4xl font-bold">{t("banner.header")}</h1>
-            <p className="text-lg text-muted-foreground max-w-2xl">{t("banner.subheader")}</p>
-          </div>
-          <Suspense fallback={null}>
-            <Main />
-          </Suspense>
-        </main>
-        <Suspense fallback={null}>
-          <Footer />
-        </Suspense>
+        <Main />
+        <Footer />
       </div>
     </div>
   );
